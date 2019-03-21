895c11175618a8840c59151ecb3cc0f5260197e0
cuicui88 <1015626181@qq.com>
2019-03-21 23:17:55 +0800

    实现对多个元素的动画控制

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import React, {Component,Fragment} from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition,TransitionGroup } from 'react-transition-group';
+
 import './style.css';
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            show:true
+            list:[]
         }
-        this.handleToggle = this.handleToggle.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
     }
     render(){
         return(
             <Fragment>
-                <CSSTransition
-                    in={this.state.show}
-                    timeout={1000}
-                    classNames='fade'
-                    unmountOnExit
-                    onEntered={(el)=>{el.style.color='blue'}}
-                    appear={true}
-                >
+                <TransitionGroup>
+                {
+                    this.state.list.map((item,index)=>{
+                        return(
+                            <CSSTransition
+                                timeout={1000}
+                                classNames='fade'
+                                unmountOnExit
+                                onEntered={(el)=>{el.style.color='blue'}}
+                                appear={true}
+                                key={index}
+                            >
+                            <div >{item}</div>
+                            </CSSTransition>
+                        )
+                    })
+                }
+                </TransitionGroup>
                 <div>hello</div>
-                </CSSTransition>
-                <button onClick={this.handleToggle}>toggle</button>
+                <button onClick={this.handleAddItem}>toggle</button>
                 
             </Fragment>
         )
     }
-    handleToggle(){
-        this.setState({
-            show: this.state.show ? false : true
+    handleAddItem(){
+        this.setState((prevState)=>{
+            return{
+                list: [...prevState.list,'item']
+            }
         })
     }
 }
