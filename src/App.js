d015b8f66b7ed5f80286867f71ec7a33270ff4d6
cuicui88 <1015626181@qq.com>
2019-03-21 23:03:30 +0800

    toggle按钮控制文字淡入淡出

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Component,Fragment} from 'react';
+import { CSSTransition } from 'react-transition-group';
+import './style.css';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            show:true
+        }
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+    render(){
+        return(
+            <Fragment>
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={1000}
+                    classNames='fade'
+                    unmountOnExit
+                    onEntered={(el)=>{el.style.color='blue'}}
+                    appear={true}
+                >
+                <div>hello</div>
+                </CSSTransition>
+                <button onClick={this.handleToggle}>toggle</button>
+                
+            </Fragment>
+        )
+    }
+    handleToggle(){
+        this.setState({
+            show: this.state.show ? false : true
+        })
+    }
 }
 
 export default App;
